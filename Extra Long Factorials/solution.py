2600877ec2e8e1685a87db4f1a834cedf5ca1874
Carmen Lam <148829232+Carmen-jx@users.noreply.github.com>
2024-01-29 18:11:16 +0000

    solved

---
+++ Extra Long Factorials/solution.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'extraLongFactorials' function below.
+#
+# The function accepts INTEGER n as parameter.
+#
+
+def extraLongFactorials(n):
+    if n < 2:
+        return 1
+    else:
+        return n * extraLongFactorials(n-1)
+        
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    num = extraLongFactorials(n)
+
+    print(num)
